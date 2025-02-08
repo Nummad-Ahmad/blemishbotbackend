11ba175ed5f0da213495641116836ef7df78ef56
nummad <nummad222@gmail.com>
2025-02-08 14:45:32 +0500

    first commit

---
+++ backend/email.js
@@ -0,0 +1,18 @@
+const { transporter } = require("./email.config");
+
+const sendVerificationCode = async (email, verificationCode) => {
+    try{
+        const response = await transporter.sendMail({
+        from: '"Blemishbot" <nummad222@gmail.com>', // sender address
+        to: email, // list of receivers
+        subject: 'Verify your email', // Subject line
+        text: "Verify your email", // plain text body
+        html: `Verify your email to get started. Your verification code is ${verificationCode}.`, // html body
+    }); 
+    console.log('Email sent', response);
+    }catch(e){
+        console.log(e);
+    }
+}
+
+module.exports = { sendVerificationCode };
